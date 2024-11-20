0852fa0708f950414b6b7fd5ffe60423ee622550
应卓 <yingzhor@gmail.com>
2024-11-20 08:28:33 +0800

    2024-11-20 08:28:33

--- src/main/java/com/github/yingzhuo/javashowcase/juc/future/CompletableFutureDemo3.java
+++ src/main/java/com/github/yingzhuo/javashowcase/juc/future/CompletableFutureDemo3.java
@@ -14,11 +14,13 @@ public class CompletableFutureDemo3 {
     public static void main(String[] args) {
         var threadPool = ThreadPoolFactories.createDefaults();
 
+        // @formatter:off
         CompletableFuture.supplyAsync(() -> "一条活鱼", threadPool)
                 .thenApply(x -> "一条去除内脏和鱼鳞的鱼")
                 .thenApply(x -> "一条被蒸熟的鱼")
                 .thenApply(x -> "一条放了调料的清蒸鱼")
                 .thenAccept(System.out::println);
+        // @formatter:on
 
         threadPool.shutdown();
     }
